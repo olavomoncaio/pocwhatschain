705761175099b7bae33e55f29fc43171616a32c8
olavomoncaio <olavo_gh.wt@hotmail.com>
2025-05-10 15:16:54 -0300

    remove env

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from langchain.llms import OpenAI
-from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
-import os
 from dotenv import load_dotenv
 from routes import process_message
 import logging
